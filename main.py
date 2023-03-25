05ad25a0cdce95a5c46c401f50f098eac6456538
Sarah Pendhari <you@example.com>
2023-03-26 00:09:01 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import string
+#Opening the file sd 
+text = open('sd.txt', encoding='utf-8').read()
+#Convert everything to lowercase
+lower_case = text.lower()
+#removing punctuations
+clean_data = lower_case.translate(str.maketrans('','',string.punctuation))
+print(clean_data)
+#Tokenisation basically means separating words form a sentence and storing them in a list
+tokenzised_words = clean_data.split()
+print(tokenzised_words)
+#stop words: don't add any emotion or have any significance in emotional analysis
+stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+final_words = []
+for i in tokenzised_words:
+    if i not in stop_words:
+        final_words.append(i)
+        
+print(final_words)
